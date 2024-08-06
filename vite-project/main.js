72aa4364025239cac9c931b066b126cdc4e4bb28
toumoon <toumoon@infoseek.jp>
2024-08-06 12:11:01 +0900

    240806 1200

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,56 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKluJeg7rF4IcV4tpxq6RUE84bFBbaXNQ",
+  authDomain: "daily-report-b7b46.firebaseapp.com",
+  projectId: "daily-report-b7b46",
+  storageBucket: "daily-report-b7b46.appspot.com",
+  messagingSenderId: "52413990038",
+  appId: "1:52413990038:web:bc4ad50d09e62494b3aeb7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+// Cloud Firestoreの初期化
+const db = getFirestore(app);
+
+
+// Cloud Firestoreから取得したデータを表示するメソッド
+const fetchHistoryDate = async () => {
+  let tags = "";
+
+  // reportsコレクションのデータを取得
+  const querySnapshot = await getDocs(collection(db,"report"));
+
+  // データをテーブル表の形式に合わせてHTMLに挿入
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+    tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().word}</td><td>${doc.data().comment}</td></tr>`
+  });
+
+  document.getElementById("js_history").innerHTML = tags;
+};
+  
+
+
+// Cloud Firestoreから取得したデータを表示するメソッドを実行する。
+if(document.getElementById("js_history")){
+  fetchHistoryDate();
+}
+
+
+
+
+
 import './style.css'  
 
 import Splide from '@splidejs/splide';
